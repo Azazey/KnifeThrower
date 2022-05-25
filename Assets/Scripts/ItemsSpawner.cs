6eb4b0cbfc580b1b83c5495feaa6dbdf130b558e
Azazey <pro1vanon@gmail.com>
2022-05-25 20:13:59 +0300

    Item spawner logic Improve

--- Assets/Scripts/ItemsSpawner.cs
+++ Assets/Scripts/ItemsSpawner.cs
@@ -8,12 +8,12 @@ public class ItemsSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _knifePrefab;
     [SerializeField] private GameObject _applePrefab;
-    [SerializeField]private int _appleSpawnChance;
-    [SerializeField]private int _knifeSpawnChance;
-    [SerializeField]private int _maxKnifes;
-    [SerializeField]private int _maxApples;
-    
-    [SerializeField] private List<ItemHolder> _spawners = new List<ItemHolder>();
+    [SerializeField] private int _appleSpawnChance;
+    [SerializeField] private int _knifeSpawnChance;
+    [SerializeField] private int _maxKnifes;
+    [SerializeField] private int _maxApples;
+
+    [SerializeField] private List<GameObject> _spawners = new List<GameObject>();
 
     private void Start()
     {
@@ -26,7 +26,7 @@ public class ItemsSpawner : MonoBehaviour
     {
         for (int i = 0; i < transform.childCount; i++)
         {
-            _spawners.Add(transform.GetChild(i).GetComponent<ItemHolder>());
+            _spawners.Add(transform.GetChild(i).gameObject);
         }
     }
 
@@ -38,21 +38,9 @@ public class ItemsSpawner : MonoBehaviour
             {
                 if (Random.Range(0, 100) < _appleSpawnChance)
                 {
-                    SpawnApples();
-                }   
+                    SpawnObject(_applePrefab);
+                }
             }
-        }
-    }
-
-    private void SpawnApples()
-    {
-        int itemHolster = Random.Range(0, _spawners.Count);
-        if (_spawners[itemHolster].Item == Items.Nothing)
-        {
-            GameObject apple = Instantiate(_applePrefab, _spawners[itemHolster].transform.position, Quaternion.identity);
-            apple.transform.rotation = _spawners[itemHolster].transform.rotation;
-            apple.transform.parent = transform.parent;
-            _spawners[itemHolster].Item = Items.Apple;
         }
     }
 
@@ -64,20 +52,24 @@ public class ItemsSpawner : MonoBehaviour
             {
                 if (Random.Range(0, 100) < _knifeSpawnChance)
                 {
-                    SpawnKnife();
-                }   
+                    SpawnObject(_knifePrefab);
+                }
             }
         }
     }
 
-    private void SpawnKnife()
+    private void SpawnObject(GameObject prefab)
     {
-        int itemHolster = Random.Range(0, _spawners.Count);
-        if (_spawners[itemHolster].Item == Items.Nothing)
+        if (_spawners.Count == 0)
         {
-            GameObject knife = Instantiate(_knifePrefab, _spawners[itemHolster].transform.position, _spawners[itemHolster].transform.rotation);
-            knife.transform.parent = transform.parent;
-            _spawners[itemHolster].Item = Items.Knife;
+            Debug.Log("Can't spawn object:" + prefab.name + "." + " Due of reached spawnpoint limit");
+            return;
         }
+
+        int itemHolster = Random.Range(0, _spawners.Count);
+        GameObject item = Instantiate(prefab, _spawners[itemHolster].transform.position,
+            _spawners[itemHolster].transform.rotation);
+        item.transform.parent = transform.parent;
+        _spawners.Remove(_spawners[itemHolster]);
     }
 }
